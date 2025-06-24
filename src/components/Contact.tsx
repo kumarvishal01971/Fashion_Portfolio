3e532cbc383dc46bb85fdc153e979c494a4a1ebd
kumarvishal01971 <xyz1234sasw@gmail.com>
2025-06-25 00:20:00 +0530

    Updated Formspree

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -22,7 +22,7 @@ const Contact = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch("https://formspree.io/f/mgvynzzw", {
+      const response = await fetch("https://formspree.io/f/mzzgkbeb", {
         method: "POST",
         headers: {
           'Accept': 'application/json',
