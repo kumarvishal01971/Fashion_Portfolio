d6617ed7d565ed6caefcab255b2aea6ad1a7734a
kumarvishal01971 <xyz1234sasw@gmail.com>
2025-06-24 16:58:25 +0530

    New update

--- src/components/PreviousCollections.tsx
+++ src/components/PreviousCollections.tsx
@@ -103,14 +103,15 @@ const PreviousCollections = () => {
       {/* Background */}
       <div className="absolute inset-0 -z-10">
         <div
-          className="w-full h-full bg-cover bg-center bg-no-repeat"
+          className="w-full h-full min-h-screen bg-cover bg-center bg-no-repeat"
           style={{
             backgroundImage: 'url(https://wallpaperaccess.com/full/5932168.jpg)',
           }}
         >
-          <div className="w-full h-full bg-slate-900/70" />
+          <div className="w-full h-full min-h-screen bg-slate-900/70" />
         </div>
       </div>
+
 
       {/* Foreground Content */}
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
